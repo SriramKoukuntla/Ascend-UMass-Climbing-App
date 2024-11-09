db8b04cba53a5b6ef20cfcce4083f4f15c7f8ea0
SidMishra19 <sidmishra@umass.edu>
2024-11-09 14:02:47 -0500

    changes

---
+++ backend/src/main/java/com/umassascend/backend/HelloController.java
@@ -0,0 +1,14 @@
+package com.umassascend.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
