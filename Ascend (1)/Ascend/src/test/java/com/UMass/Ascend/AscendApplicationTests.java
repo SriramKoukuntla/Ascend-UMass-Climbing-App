c21a7dadbdb0812e51eeb4accd00dced33b17541
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2024-11-09 03:12:09 -0500

    SpringBoot SetUp

---
+++ Ascend (1)/Ascend/src/test/java/com/UMass/Ascend/AscendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UMass.Ascend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
