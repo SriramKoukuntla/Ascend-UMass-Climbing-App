f4d5190f57216195a1fadf8c750422905af0dbd7
SidMishra19 <sidmishra@umass.edu>
2024-11-10 06:06:33 -0500

    msgs

--- hackumass/hackumass/src/main/java/com/example/ascend/hackumass/controller/MasterController.java
+++ hackumass/hackumass/src/main/java/com/example/ascend/hackumass/controller/MasterController.java
@@ -33,36 +33,13 @@ public class MasterController {
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
-        return userAuthentication.login(user.getUsername(), user.getPassword(), user.isStaff());
+        return userBase.login(user.getUsername(), user.getPassword(), user.isStaff());
     }
 
-    //History/Elo/Rank
-   @PostMapping("/addScore")
-   public void addScore(@RequestBody User user, int score) {
-       leaderboard.addScore(user, score);
-   }
-
-   @PostMapping("/removeScore")
-   public void removeScore(@RequestBody User user, int score) {
-       leaderboard.removeScore(user);
-   }
-
-    @GetMapping("/top10")
-    public List<Map.Entry<User, Integer>> getTop10Users() {
-        return leaderboard.getTop10Users();
-    }  
-
-    // Endpoint to add a post to the forum
-    @PostMapping("/add")
-    public String addPost(@RequestBody String text) {
-        forum.addPost(text);
-        return "Post added successfully";
-    }
-
-    // Endpoint to get all posts from the forum
-    @GetMapping("/posts")
-    public List<String> getAllPosts() {
-        return forum.getAllPosts();
+    // History/Elo/Rank
+    @PostMapping("/routeRequest")
+    public String requestRoute(@RequestBody User user) {
+        return userBase.login(user.getUsername(), );
     }
 
 }
