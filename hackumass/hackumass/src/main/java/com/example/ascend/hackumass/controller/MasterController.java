7dc21edd2d03dc16c6a38d7ba8210e589f792756
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2024-11-10 00:01:27 -0500

    Correct User Authentification, but without salting and hashing

--- hackumass/hackumass/src/main/java/com/example/ascend/hackumass/controller/MasterController.java
+++ hackumass/hackumass/src/main/java/com/example/ascend/hackumass/controller/MasterController.java
@@ -5,11 +5,12 @@ import com.example.ascend.hackumass.model.UserAuthentication;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class MasterController {
+
+    private final UserAuthentication userAuthentication = new UserAuthentication();
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
